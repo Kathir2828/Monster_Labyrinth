4703eb5f3349c56d94ef0d43d20c89d15da37be0
kathir <kathirsrivathsanofc@gmail.com>
2026-07-23 22:26:23 +0530

    last commit

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://dungeon-laybriths.web.app/");
+  res.header("Access-Control-Allow-Origin", "https://dungeon-laybriths.web.app");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   // possing the control
